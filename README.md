185801823b5f02bd4f5eb93a00e479a09b147067
dedi <dedi.hir@gmail.com>
2016-11-25 11:29:36 +0200

    Fixed a bug in gxref-create-db, and expanded documentation a bit.
    gxref-create-db fix description: made it use the directory provided as
    an argument, and not the default gxref project directory.

--- README.md
+++ README.md
@@ -6,6 +6,15 @@
 A pretty simple (but, at least for me, effective) backend for Emacs 25
 xref library using GNU Global.
 
+## Overview
+
+Emacs 25 introduces a new (and experimental) `xref' package.  The
+package aims to provide a standardized access to cross-referencing
+operations, while allowing the implementation of different back-ends
+which implement that cross-referencing using different mechanisms.  The
+gxref package implements an xref backend using the GNU Global tool.
+
+
 ## Prerequisites:
 
 * GNU Global.
@@ -32,19 +41,19 @@ Add something like the following to your init.el file:
 (add-to-list 'xref-backend-functions 'gxref-xref-backend)
 ```
 
-This will add gxref as a backend for xref functions.  The backend will
-be used whenever a GNU Global project is detected.  That is, whenever
-a GTAGS file exists in the current directory or above it, or an
-explicit project [was set](#setting_project).
+This will add gxref as a backend for xref functions.  The backend
+will be used whenever a GNU Global project is detected.  That is,
+whenever a GTAGS database file exists in the current directory or
+above it, or an explicit project [was set](#setting_project).
 
 
 ## <a name="usage"></a>Usage
 
 ### Using gxref to locate tags
 
-After [setup](#setup), invoking any of the xref functions will use GNU
-Global to locate the tag whenever a GTAGS file can be located.  By
-default, xref functions are bound as follows:
+After [setup](#setup), invoking any of the xref functions will use
+GNU Global whenever a GTAGS file can be located.  By default, xref
+functions are bound as follows:
 
 
 | Function              | Binding  |
@@ -60,10 +69,10 @@ fall back to whatever other backends it's configured to try.
 ### <a name="setting_project"></a>Project root directory.
 
 By default, gxref searches for the root directory of the project,
-which contains the GTAGS file, by looking for that file in the current
-directory, and then upwards through parent directories until it is
-located.  If you prefer, you can explicitly set the project directory
-manually.  This can be done either interactively, by typing `M-x
+and the GTAGS database file, by looking in the current directory,
+and then upwards through parent directories until the database is located.
+If you prefer, you can explicitly set the project directory.
+This can be done either interactively, by typing `M-x
 gxref-set-project-dir RET`, or by setting the variable
 `gxref-gtags-root-dir` to the GTAGS directory.  You can also set up
 `gxref-gtags-root-dir` as a file-local or a dir-local variable.
@@ -73,8 +82,8 @@ gxref-set-project-dir RET`, or by setting the variable
 gxref can be customized in several ways.  use
 `M-x customize-group RET gxref RET` to start.
 
-Additionally, the following variables can be defined to tweak the
-behaviour.  You can set them either globally, or as file-local or
+Additionally, the following variables can be used to affect the execution
+of GNU Global. You can set them either globally, or as file-local or
 dir-local variables:
 
  - gxref-gtags-conf
--- gxref.el
+++ gxref.el
@@ -26,6 +26,15 @@
 ;; A pretty simple (but, at least for me, effective) backend for Emacs 25
 ;; xref library using GNU Global.
 
+;; ## Overview
+
+;; Emacs 25 introduces a new (and experimental) `xref' package.  The
+;; package aims to provide a standardized access to cross-referencing
+;; operations, while allowing the implementation of different back-ends
+;; which implement that cross-referencing using different mechanisms.  The
+;; gxref package implements an xref backend using the GNU Global tool.
+
+
 ;; ## Prerequisites:
 
 ;; * GNU Global.
@@ -52,19 +61,19 @@
 ;; (add-to-list 'xref-backend-functions 'gxref-xref-backend)
 ;; ```
 
-;; This will add gxref as a backend for xref functions.  The backend will
-;; be used whenever a GNU Global project is detected.  That is, whenever
-;; a GTAGS file exists in the current directory or above it, or an
-;; explicit project [was set](#setting_project).
+;; This will add gxref as a backend for xref functions.  The backend
+;; will be used whenever a GNU Global project is detected.  That is,
+;; whenever a GTAGS database file exists in the current directory or
+;; above it, or an explicit project [was set](#setting_project).
 
 
 ;; ## <a name="usage"></a>Usage
 
 ;; ### Using gxref to locate tags
 
-;; After [setup](#setup), invoking any of the xref functions will use GNU
-;; Global to locate the tag whenever a GTAGS file can be located.  By
-;; default, xref functions are bound as follows:
+;; After [setup](#setup), invoking any of the xref functions will use
+;; GNU Global whenever a GTAGS file can be located.  By default, xref
+;; functions are bound as follows:
 
 
 ;; | Function              | Binding  |
@@ -80,10 +89,10 @@
 ;; ### <a name="setting_project"></a>Project root directory.
 
 ;; By default, gxref searches for the root directory of the project,
-;; which contains the GTAGS file, by looking for that file in the current
-;; directory, and then upwards through parent directories until it is
-;; located.  If you prefer, you can explicitly set the project directory
-;; manually.  This can be done either interactively, by typing `M-x
+;; and the GTAGS database file, by looking in the current directory,
+;; and then upwards through parent directories until the database is located.
+;; If you prefer, you can explicitly set the project directory.
+;; This can be done either interactively, by typing `M-x
 ;; gxref-set-project-dir RET`, or by setting the variable
 ;; `gxref-gtags-root-dir` to the GTAGS directory.  You can also set up
 ;; `gxref-gtags-root-dir` as a file-local or a dir-local variable.
@@ -93,8 +102,8 @@
 ;; gxref can be customized in several ways.  use
 ;; `M-x customize-group RET gxref RET` to start.
 
-;; Additionally, the following variables can be defined to tweak the
-;; behaviour.  You can set them either globally, or as file-local or
+;; Additionally, the following variables can be used to affect the execution
+;; of GNU Global. You can set them either globally, or as file-local or
 ;; dir-local variables:
 
 ;;  - gxref-gtags-conf
@@ -300,9 +309,9 @@ If DISPLAY is true, display the process buffer.  Return the new process."
   "Create a GTAGS database in the directory specified as PROJECT-ROOT-DIR."
   (interactive "DCreate db in directory: ")
     (set-process-sentinel
-     (gxref--create-db-internal gxref-project-root-dir)
+     (gxref--create-db-internal project-root-dir)
      (lambda (_process event)
-       (message "Gxref tag %s: %s" project-dir
+       (message "Gxref tag %s: %s" project-root-dir
                 (replace-regexp-in-string "\n+$" "" event)))))
 
 
